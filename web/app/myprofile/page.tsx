8e8f579cdb876da0f156eece77151eb270797f9f
JeromeJULIEN <102427619+JeromeJULIEN@users.noreply.github.com>
2024-07-15 14:40:52 +0200

    feat: menu OK with basic pages for all routes

---
+++ web/app/myprofile/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// type Props = {}
+
+const MyProfilPage = (/*props: Props*/) => {
+  return (
+    <div>MyProfilPage</div>
+  )
+}
+
+export default MyProfilPage
---
+++ web/app/rewardsmarket/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// type Props = {}
+
+const RewardMarketPage = (/*props: Props*/) => {
+  return (
+    <div>RewardMarketPage</div>
+  )
+}
+
+export default RewardMarketPage
