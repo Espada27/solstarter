63b539284b1ca35f9abd7942b10cd4220ae18b52
bendog <bpoulinet@gmail.com>
2024-07-15 18:16:23 +0200

    del empty errors file

--- anchor/programs/solstarter/src/errors.rs
+++
@@ -1,9 +0,0 @@
-use anchor_lang::prelude::*;
-
-#[error_code]
-pub enum CreateProjectError {
-    #[msg("Project already exists")]
-    AlreadyExists,
-    #[msg("Invalid parameters")]
-    InvalidParameters,
-}
--- anchor/programs/solstarter/src/lib.rs
+++ anchor/programs/solstarter/src/lib.rs
@@ -1,6 +1,5 @@
 use anchor_lang::prelude::*;
 
-pub mod errors;
 pub mod instructions;
 pub mod state;
 
