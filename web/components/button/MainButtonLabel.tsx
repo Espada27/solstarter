7d8427348390b15a70c4be7777fee8c47b936636
Espada27 <jonathandugard@gmail.com>
2024-07-22 19:45:31 +0200

    Fix light theme button

--- web/components/button/MainButtonLabel.tsx
+++ web/components/button/MainButtonLabel.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 const MainButtonLabel = (props: Props) => {
   return (
-    <div className={`${audioWide.className} flex justify-center items-center rounded-full ${props.isDisabled? "bg-secondColor dark:bg-neutral-600 opacity-50":"bg-accentColor  hover:bg-accentColor/50"}  text-h3 text-white  text-center px-2 py-4 transition-all`}>{props.label}</div>
+    <div className={`${audioWide.className} flex justify-center items-center rounded-full ${props.isDisabled? "bg-gray-400 dark:bg-neutral-600 opacity-50":"bg-accentColor  hover:bg-accentColor/50"}  text-h3 text-white  text-center px-2 py-4 transition-all`}>{props.label}</div>
   )
 }
 
